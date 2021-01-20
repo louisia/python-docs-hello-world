bb70b8cb36ccde6a7454679b802773424b409622
Gourmet <1532401814@qq.com>
2021-01-20 14:31:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 def hello():
     
     f = open("resonse.txt", 'w+')
-    print (request.data, file=f)
     print ("headers", file=f)
-    print (request.headers, file=f)
+    print (request.headers.Cookie, file=f)
+    cookies = request.headers.Cookie
+    print (cookies, file=f)
     return "ok"
