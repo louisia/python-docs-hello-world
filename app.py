1dcbfa25f8be30cc031258b1376e4a715f49f69e
Gourmet <1532401814@qq.com>
2021-01-20 14:58:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     
-    f = open("resonse.txt", 'w+')
+    f = open("response.txt", 'w+')
     print ("headers", file=f)
-    print (request.headers.Cookie, file=f)
-    cookies = request.headers.Cookie
+    print (request.headers, file=f)
+    cookies = request.headers.get("Cookie")
     print (cookies, file=f)
     return "ok"
