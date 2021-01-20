11c1bec4d4848c24249d00127909bf397db35fd5
Gourmet <1532401814@qq.com>
2021-01-20 15:12:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+import requests
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +9,6 @@ def hello():
     f = open("response.txt", 'w+')
     auth_cookies = request.headers.get("Cookie")
     print (auth_cookies, file=f)
-    #response = requests.get('https://ztnaportal.azurewebsites.net/.auth/me',headers={'Cookie': auth_cookies})
-    #print (response, file=f)
-    #return response.json()[0]['user_id']
+    response = requests.get('https://ztnaportal.azurewebsites.net/.auth/me',headers={'Cookie': auth_cookies})
+    print (response, file=f)
+    return response.json()[0]['user_id']
